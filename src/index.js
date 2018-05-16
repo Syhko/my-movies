220c18c65f90897471817d5bd5a3d61bf8adb0d5
Alexandre Grauby <alexandregrauby@gmail.com>
2018-05-16 15:24:18 +0200

    Adding react-router-dom package

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>,
+  document.getElementById('root')
+)
 registerServiceWorker();
