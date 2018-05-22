ff79fc1ee9bf366cb7b89cca7c6138932b230d8b
Alexandre Grauby <alexandregrauby@gmail.com>
2018-05-22 15:58:29 +0200

    Adding sessions with firebase

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import base from './base';
 
 
 class App extends PureComponent {
+
   state = {
     movies: {},
     showMovieFocus: false,
@@ -31,7 +32,7 @@ class App extends PureComponent {
 
   // Synchro with firebase
   componentWillMount() {
-    this.ref = base.syncState('/movies', {
+    this.ref = base.syncState(`${this.props.match.params.pseudo}/movies`, {
       context: this,
       state: 'movies',
     });
@@ -100,7 +101,7 @@ class App extends PureComponent {
     return (
 
       <div className="App">
-        <Header onChange={value => this.setState({ searchText: value })}/>
+        <Header pseudo={this.props.match.params.pseudo} onChange={value => this.setState({ searchText: value })}/>
         <div className="grid">
           <MovieForm createMovie={this.props.createMovie} addMovie={this.addMovie} />
           {movieList}
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ ReactDOM.render(
   <Router>
     <div>
       <Route exact path="/" component={Connexion} />
-      <Route path="/movies/" component={App} />
+      <Route path="/movies/:pseudo" render={(props) => <App {...props} />}/>
     </div>
   </Router>,
   document.getElementById('root'),
