3db91778c19c22464ca11d1fb6abefde676d20c9
Alexandre Grauby <alexandregrauby@gmail.com>
2018-05-31 10:45:16 +0200

    Adding first version of real time input search on MovieForm

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Autosuggest from 'react-autosuggest';
 import './MovieForm.css';
 
 class MovieForm extends React.Component {
@@ -13,7 +14,50 @@ class MovieForm extends React.Component {
     writer: '',
     plot: '',
     ratings: '',
+    value: '',
+    listMovies: [],
+    suggestions: []
   }
+
+  onChange = (event, { newValue }) => {
+    this.setState({
+      value: newValue
+    });
+    this.requestFetchSuggestion();
+  };
+
+  getSuggestions = (value) => {
+    const inputValue = value.toLowerCase();
+    const inputLength = inputValue.length;
+
+    return this.state.listMovies[0] !== [] && inputLength === 0 ? [] : this.state.listMovies.filter(list =>
+      list.Title.toLowerCase().slice(0, inputLength) === inputValue
+    );
+  };
+
+  getSuggestionValue = (suggestion) => {
+    return suggestion.Title;
+  }
+
+  renderSuggestion = (suggestion) => {
+    return (
+      <div>
+        {suggestion.Title}
+      </div>
+    );
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+  };
 
   createMovie = async (event) => {
     event.preventDefault();
@@ -37,11 +81,24 @@ class MovieForm extends React.Component {
     this.addForm.reset();
   }
 
+  requestFetchSuggestion = () => {
+    const currentComponent = this;
+
+    this.state.value.length>=3 && fetch(`https://www.omdbapi.com/?apikey=ffc03c92&s=${this.state.value}`)
+    .then(response => response.json())
+    .then((data) => {
+      if (data.Response !== "False") {
+        const dataArray=Object.values(data).slice(0,1);
+      return currentComponent.setState ({ listMovies : dataArray[0] })
+    }
+      })
+
+  }
+
   requestFetch = () => {
     const currentComponent = this;
-    const param = this.imageLink.value;
 
-    return fetch(`https://www.omdbapi.com/?apikey=ffc03c92&t=${param}`)
+    return fetch(`https://www.omdbapi.com/?apikey=ffc03c92&t=${this.state.value}`)
       .then(response => response.json())
       .then((data) => {
         const title = data.Title;
@@ -61,12 +118,27 @@ class MovieForm extends React.Component {
   }
 
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'Search a movie',
+      value,
+      onChange: this.onChange
+    };
+    console.log(this.state.suggestions);
+
     return (
       <div className="ficheForm">
         <form className="addForm" ref={input => this.addForm = input} onSubmit={e => this.createMovie(e)} >
           <h1>HERE</h1>
           <p> ADD A MOVIE/SERIE TO YOUR DATABASE </p>
-          <input required className="imageLink" placeholder="Title..." ref={input => this.imageLink = input} />
+          <Autosuggest
+            suggestions={suggestions}
+            onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+            getSuggestionValue={this.getSuggestionValue}
+            renderSuggestion={this.renderSuggestion}
+            inputProps={inputProps}
+          />
           <button type="submit" className="addButton">Add</button>
           <p>(Click on a movie to see details)</p>
         </form>
