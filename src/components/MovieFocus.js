c8f8ab56c296b65c6d129e7ad99972807b0fdad8
Alexandre Grauby <alexandregrauby@gmail.com>
2018-07-03 08:34:05 +0200

    switching MovieFocus to a function component

--- src/components/MovieFocus.js
+++ src/components/MovieFocus.js
@@ -1,35 +1,28 @@
 import React from 'react';
 import './MovieFocus.css';
 
-class MovieFocus extends React.Component {
-  render() {
-    const {
-      poster, plot, ratings, title, actors, date, awards, genre, writer, isChecked
-    } = this.props;
-
-
-    return (
-      <div className="wrapper">
-        <div className="row">
-          <div className="row_cell">
-            <img className="imageFocus" src={poster} alt="" />
-          </div>
-          <div className="row_cell">
-            <p className="content"> <span style={{ fontWeight: 'bold' }}>Title : </span>{title} </p>
-            <p className="content"> <span style={{ fontWeight: 'bold' }}>Year : </span>{date} </p>
-            <p className="content"> <span style={{ fontWeight: 'bold' }}>Genre : </span>{genre} </p>
-          </div>
+function MovieFocus(props) {
+  return (
+    <div className="wrapper">
+      <div className="row">
+        <div className="row_cell">
+          <img className="imageFocus" src={props.poster} alt="" />
         </div>
-        <div className="row_plot">
-          <p style={{ fontWeight: 'bold' }}> {plot} </p>
-        </div>
-        <div className="footer">
-          <p className="rates" style={{ fontWeight: 'bold' }}>IMDB : {ratings} </p>
+        <div className="row_cell">
+          <p className="content"> <span style={{ fontWeight: 'bold' }}>Title : </span>{props.title} </p>
+          <p className="content"> <span style={{ fontWeight: 'bold' }}>Year : </span>{props.date} </p>
+          <p className="content"> <span style={{ fontWeight: 'bold' }}>Genre : </span>{props.genre} </p>
         </div>
       </div>
+      <div className="row_plot">
+        <p style={{ fontWeight: 'bold' }}> {props.plot} </p>
+      </div>
+      <div className="footer">
+        <p className="rates" style={{ fontWeight: 'bold' }}>IMDB : {props.ratings} </p>
+      </div>
+    </div>
 
-    );
-  }
+  );
 }
 
 export default MovieFocus;
